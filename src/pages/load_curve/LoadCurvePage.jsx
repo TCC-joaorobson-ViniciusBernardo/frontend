9aa96c5181761cf5356798f9c5c092ac91ee83bb
Vinicius Lima <vfbdelima@gmail.com>
2021-09-29 23:21:41 -0300

    Add initial structure of main features pages
    Signed-off-by: Vinicius Lima <vfbdelima@gmail.com>

---
+++ src/pages/load_curve/LoadCurvePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadCurvePage = () => {
+  return(
+    <div>
+      <h1>Curvas de Carga</h1>
+    </div>
+  );
+}
+
+export default LoadCurvePage;
---
+++ src/pages/sandbox/SandboxPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SandboxPage = () => {
+  return(
+    <div>
+      <h1>Sandbox</h1>
+    </div>
+  );
+}
+
+export default SandboxPage;
